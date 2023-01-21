7d0a6b38e574703a217f54739ed0ff42035e2d00
yingluosanqian <yingluosanqian@gmail.com>
2023-01-21 15:48:58 +0800

    15:48

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-swkwd: swkwd_lexer.l swkwd_parser.y ast.h
-       bison -d swkwd_parser.y
-       flex -o swkwd_lexer.lex.c swkwd_lexer.l
-       gcc -o $@ swkwd_parser.tab.c swkwd_lexer.lex.c ast.c
+bison -d swkwd.y
+flex -oswkwd.lex.c swkwd.l
+gcc -o swkwd swksw.tab.c swkwd.lex.c ast.c
--- swkwd.y
+++ swkwd.y
@@ -1,7 +1,7 @@
 %{
 #include <stdio.h>
 #include <stdlib.h>
-#include <ast.h>
+#include "ast.h"
 %}
 
 %union {
